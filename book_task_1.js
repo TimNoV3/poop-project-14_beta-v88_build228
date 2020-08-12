b33dac60e84389ed032f20e423d1e0722cae5b14
TimNoV3 <tilichev@mail.ru>
2020-08-12 12:21:07 +0300

    replace indexOF

---
+++ book_task_1.js
@@ -0,0 +1,16 @@
+function task_1(){
+  let a = ""
+  for (let number = 0; number < 7; number+=1){
+    a += "#";
+    console.log(a);
+  }
+}
+task_1();
+
+function task_2(){
+  for (let number = 1; number <= 100; number+=1){
+    if (number % 3 == 0) {
+      
+    }
+  }
+}
