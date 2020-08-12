931b7973ce7c03dfc212a6fe764f12e4cbfb396f
TimNoV3 <tilichev@mail.ru>
2020-08-12 13:32:39 +0300

    book's tasks

--- book_task_1.js
+++ book_task_1.js
@@ -9,8 +9,25 @@ task_1();
 
 function task_2(){
   for (let number = 1; number <= 100; number+=1){
-    if (number % 3 == 0) {
-      
+    if (number % 3 == 0 && number % 5 == 0) console.log('FizzBuzz')
+    else if (number % 3 == 0) console.log('Fizz')
+    else if (number % 5 == 0) console.log('Buzz')
+    else {
+      console.log(number);
     }
   }
 }
+
+task_2();
+
+function task_3(n,m) {
+  for (let i = 0; i < n; i+=1) {
+    let str = i % 2 == 0 ? "":" ";
+    for (let j = 0; j < m; j+=1) {
+      str+="# ";
+    }
+    console.log(str);
+  }
+}
+
+task_3(8,8);
