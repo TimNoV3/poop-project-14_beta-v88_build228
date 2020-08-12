13687e6d0d0af2092eb0ed42d37198d6809d4589
TimNoV3 <tilichev@mail.ru>
2020-08-12 13:40:13 +0300

    replace ==

--- books_task/book_task_1.js
+++ books_task/book_task_1.js
@@ -9,9 +9,9 @@ task_1();
 
 function task_2(){
   for (let number = 1; number <= 100; number+=1){
-    if (number % 3 == 0 && number % 5 == 0) console.log('FizzBuzz')
-    else if (number % 3 == 0) console.log('Fizz')
-    else if (number % 5 == 0) console.log('Buzz')
+    if (number % 3 === 0 && number % 5 === 0) console.log('FizzBuzz')
+    else if (number % 3 === 0) console.log('Fizz')
+    else if (number % 5 === 0) console.log('Buzz')
     else {
       console.log(number);
     }
@@ -22,7 +22,7 @@ task_2();
 
 function task_3(n,m) {
   for (let i = 0; i < n; i+=1) {
-    let str = i % 2 == 0 ? "":" ";
+    let str = i % 2 === 0 ? "":" ";
     for (let j = 0; j < m; j+=1) {
       str+="# ";
     }
